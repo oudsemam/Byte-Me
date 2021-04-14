469ad32f5368cc37584f890ffd355f963283c777
Mechial <mechial.harrison@gmail.com>
2021-04-14 19:03:41 -0400

    Major components added

--- src/app/app.module.ts
+++ src/app/app.module.ts
@@ -3,10 +3,34 @@ import { BrowserModule } from '@angular/platform-browser';
 
 import { AppComponent } from './app.component';
 import { AppRoutingModule } from './app-routing.module';
+import { LoginComponent } from './login/login.component';
+import { MainLayoutComponent } from './main-layout/main-layout.component';
+import { ExploreComponent } from './explore/explore.component';
+import { CatagoriesComponent } from './catagories/catagories.component';
+import { ResultsComponent } from './results/results.component';
+import { RecipeViewComponent } from './recipe-view/recipe-view.component';
+import { IngredientsComponent } from './ingredients/ingredients.component';
+import { DirectionsComponent } from './directions/directions.component';
+import { NotesComponent } from './notes/notes.component';
+import { FavoritesComponent } from './favorites/favorites.component';
+import { ConversionsComponent } from './conversions/conversions.component';
+import { ShoppingListComponent } from './shopping-list/shopping-list.component';
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    LoginComponent,
+    MainLayoutComponent,
+    ExploreComponent,
+    CatagoriesComponent,
+    ResultsComponent,
+    RecipeViewComponent,
+    IngredientsComponent,
+    DirectionsComponent,
+    NotesComponent,
+    FavoritesComponent,
+    ConversionsComponent,
+    ShoppingListComponent
   ],
   imports: [
     BrowserModule,

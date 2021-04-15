d1aef21e6f71333fe81f1fc6c4ccfaea532632d9
Mechial <mechial.harrison@gmail.com>
2021-04-15 19:59:56 -0400

    api request now supports search with dietary restrictions, catagories, and cuisine.

--- src/app/explore/explore.component.ts
+++ src/app/explore/explore.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit } from '@angular/core';
 import {Observable} from 'rxjs';
+import { FileDetector } from 'selenium-webdriver';
 import { RecipiesService} from '../recipes.service'
 @Component({
   selector: 'app-explore',
@@ -10,14 +11,38 @@ export class ExploreComponent implements OnInit {
 
   recipes: Observable<any> | null = null;
   searchText: string = ''
+  vegan: boolean = false
+  vegatarian: boolean = false
+  glutenFree: boolean = false
+  dairyFree: boolean = false
+  keto: boolean = false
+  catagory: string = ""
+  cuisine: string = ""
 
   constructor(private service:RecipiesService) { }
 
   ngOnInit(): void {
   }
 
-  searchByTerm(){
-    this.recipes = this.service.searchRecipiesByTerm(this.searchText)
+  search(){
+    let healthfilters: string[] = []
+    if (this.vegan){
+      healthfilters.push('vegan')
+    }
+    if (this.vegatarian){
+      healthfilters.push('vegatarian')
+    }
+    if (this.glutenFree){
+      healthfilters.push('gluten-free')
+    }
+    if (this.dairyFree){
+      healthfilters.push('dairy-free')
+    }
+    if (this.keto){
+      healthfilters.push('keto-friendly')
+    }
+    
+    this.recipes = this.service.searchRecipies(this.searchText, healthfilters, this.catagory, this.cuisine)
    
   }
 }
--- src/app/recipes.service.ts
+++ src/app/recipes.service.ts
@@ -10,7 +10,19 @@ export class RecipiesService {
 
 
   //Retruns list of recipies matching search term in form
-  searchRecipiesByTerm(searchTerm: string){
-    return this.http.get(`https://api.edamam.com/search?q=${searchTerm}&to=30&app_id=a8edfa33&app_key=b218030c46c79eea9911f87e1c55b759`)
+  searchRecipies( searchTerm: string, dietSelection: string[], catagory: string, cuisine: string){
+    let options = ""
+    let catagoryInput = ""
+    if(catagory != ""){let catagoryInput = `&${catagory}`}
+
+    let cuisineInput = ""
+    if(cuisine!= ""){
+      cuisineInput = `&${cuisine}`
+    }
+    for(let option of dietSelection){
+      options += `&health=${option}`
+    }
+    return this.http.get(`https://api.edamam.com/search?q=${searchTerm}${options}${catagoryInput}${cuisineInput}&to=30&app_id=a8edfa33&app_key=b218030c46c79eea9911f87e1c55b759`)
   }
 }
+

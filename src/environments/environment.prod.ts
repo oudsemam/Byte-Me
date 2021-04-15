820a5fd64a5ccadb1b537043b3a2624472fe1d8d
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-15 19:10:49 -0400

    new branch

--- src/environments/environment.prod.ts
+++ src/environments/environment.prod.ts
@@ -1,13 +1,5 @@
 export const environment = {
   production: true
 
-  firebaseConfig = {
-    apiKey: "AIzaSyDV0mhNueiLDuQWUtAfvq2uiN0LiqtJHn0",
-    authDomain: "byte-me-recipe-app.firebaseapp.com",
-    projectId: "byte-me-recipe-app",
-    storageBucket: "byte-me-recipe-app.appspot.com",
-    messagingSenderId: "728385953403",
-    appId: "1:728385953403:web:8f5047e5c25fc94a3f4cde",
-    measurementId: "G-BVMGNYSV8R"
-  };
+
 };

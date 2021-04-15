75c11fb3ed54d8badc417c076936ce858a373abb
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-14 22:48:42 -0400

    route guard in angular routes

--- src/app/app-routing.module.ts
+++ src/app/app-routing.module.ts
@@ -6,25 +6,23 @@ import { SignInComponent } from '../app/FireBase/sign-in/sign-in.component';
 import { SignUpComponent } from '../app/FireBase/sign-up/sign-up.component';
 import { DashboardComponent } from '../app/FireBase/dashboard/dashboard.component';
 import { ForgotPasswordComponent } from '../app/FireBase/forgot-password/forgot-password.component';
-import { AuthGuard } from '../../node_modules/@angular/fire/auth-guard/auth-guard';
+import { AuthGuard } from '../app/shared/guard/auth.guard';
 import { VerifyEmailComponent } from '../app/FireBase/verify-email/verify-email.component';
-import { Routes } from '@angular/router';
+import { Routes, RouterModule } from '@angular/router';
 
 const FireBaseRoutes: Routes = [
   { path: '', redirectTo: '/sign-in', pathMatch: 'full' },
   { path: 'sign-in', component: SignInComponent },
   { path: 'register-user', component: SignUpComponent },
-  { path: 'dashboard', component: DashboardComponent },
+  {path: 'dashboard',component: DashboardComponent,
+canActivate: [AuthGuard],},
   { path: 'forgot-password', component: ForgotPasswordComponent },
   { path: 'verify-email-address', component: VerifyEmailComponent },
 ];
 
 @NgModule({
   declarations: [],
-  imports: [
-    RouterModule.forRoot(routes),
-    CommonModule,
-    
-  ]
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule],
 })
-export class AppRoutingModule { }
+export class AppRoutingModule {}

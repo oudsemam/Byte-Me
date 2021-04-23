bc37a8730946e0878f07eb3f979a348a65bd3b8f
pbla2394@gmail.com <pbla2394@gmail.com>
2021-04-22 22:28:01 -0400

    Added structure for shopping list

--- src/app/shopping-list/shopping-list.component.ts
+++ src/app/shopping-list/shopping-list.component.ts
@@ -1,15 +1,18 @@
 import { Component, OnInit } from '@angular/core';
-
+import {
+  faPenSquare,
+  faTrash,
+  faPlus,
+} from '@fortawesome/free-solid-svg-icons';
 @Component({
   selector: 'app-shopping-list',
   templateUrl: './shopping-list.component.html',
-  styleUrls: ['./shopping-list.component.css']
+  styleUrls: ['./shopping-list.component.css'],
 })
 export class ShoppingListComponent implements OnInit {
-
-  constructor() { }
-
-  ngOnInit(): void {
-  }
-
+  faPenSquare = faPenSquare;
+  faTrash = faTrash;
+  faPlus = faPlus;
+  constructor() {}
+  ngOnInit(): void {}
 }

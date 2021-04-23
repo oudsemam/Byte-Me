1e08a5c0c4712ab64b79f3545e967d8bf7265a6f
Mechial <mechial.harrison@gmail.com>
2021-04-22 21:44:58 -0400

    switch

--- src/app/search-bar/search-bar.component.ts
+++ src/app/search-bar/search-bar.component.ts
@@ -33,7 +33,6 @@ export class SearchBarComponent implements OnInit, OnDestroy {
   
 
   search() {
-    throw new Error("Invalid Search")
     let healthfilters: string[] = [];
     if (this.vegan) {
       healthfilters.push('vegan');

4e80a26a01c7d720ae58bdaf0e63a71a11b947f7
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-24 16:58:16 -0400

    added abrivations onto  conversion real quick (noticed they were missing) still updating how favorites are working

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -14,19 +14,19 @@ export class FavoritesComponent implements OnInit {
   status = 'Enable';
   recipeList: Observable<any> | null = null;
   searchTerm: string | null = null;
-  diet
+  dietSelection: string[] | null = null;
+  catagory: string | null = null;
+  cuisine: string | null = null;
 
   constructor(private service: RecipesService) {}
 
   ngOnInit(): void {
-  }
-
-  enableDisable() {
-    this.toggle = !this.toggle;
-    this.status = this.toggle ? 'Enable' : 'Disable';
+    this.recipeList = this.service.searchRecipes(this.searchTerm, this.dietSelection, this.catagory, this.cuisine)
   }
 
   saveRecipe(recipeItem: any){
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? 'Enable' : 'Disable';
     if (!recipeItem.favorite) {
       recipeItem.favorite = true;
       this.service.addRecipeBookListEvent(recipeItem);

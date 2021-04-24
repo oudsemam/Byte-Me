0ec04bbfd7bcff5a4883fc034089c729c35d14de
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-24 17:01:23 -0400

    moved enableDisable into its own function and called it in saveRecipe function

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -14,19 +14,24 @@ export class FavoritesComponent implements OnInit {
   status = 'Enable';
   recipeList: Observable<any> | null = null;
   searchTerm: string | null = null;
-  dietSelection: string[] | null = null;
+  dietSelection: string [] | null = null;
   catagory: string | null = null;
   cuisine: string | null = null;
 
   constructor(private service: RecipesService) {}
 
   ngOnInit(): void {
+    // not 100 sure if we need this but going to try
     this.recipeList = this.service.searchRecipes(this.searchTerm, this.dietSelection, this.catagory, this.cuisine)
   }
 
-  saveRecipe(recipeItem: any){
+  enableDisable(){
     this.toggle = !this.toggle;
     this.status = this.toggle ? 'Enable' : 'Disable';
+  }
+
+  saveRecipe(recipeItem: any){
+    this.enableDisable();
     if (!recipeItem.favorite) {
       recipeItem.favorite = true;
       this.service.addRecipeBookListEvent(recipeItem);

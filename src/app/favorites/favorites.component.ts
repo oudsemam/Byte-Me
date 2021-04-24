940c098b4a6faeaef20eb7e89b6de44cc87b8963
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-24 13:18:35 -0400

    changed the heart to its own component in favorites

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,15 +1,22 @@
 import { Component, OnInit } from '@angular/core';
+import { faHeart } from '@fortawesome/free-solid-svg-icons';
 
 @Component({
   selector: 'app-favorites',
   templateUrl: './favorites.component.html',
-  styleUrls: ['./favorites.component.css']
+  styleUrls: ['./favorites.component.css'],
 })
 export class FavoritesComponent implements OnInit {
+  faHeart = faHeart;
+  toggle: boolean = false;
+  status = 'Enable';
 
-  constructor() { }
+  constructor() {}
 
-  ngOnInit(): void {
+  ngOnInit(): void {}
+
+  enableDisable() {
+    this.toggle = !this.toggle;
+    this.status = this.toggle ? 'Enable' : 'Disable';
   }
-
 }

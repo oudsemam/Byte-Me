495f6d196c7155e7a73816919911fa49fe4f77ac
Maggie Oudsema <maggieoudsema@gmail.com>
2021-04-24 16:37:52 -0400

    working on formulas and wire framed the recipe book for favorites... still a work in progress

--- src/app/favorites/favorites.component.ts
+++ src/app/favorites/favorites.component.ts
@@ -1,5 +1,8 @@
 import { Component, OnInit } from '@angular/core';
 import { faHeart } from '@fortawesome/free-solid-svg-icons';
+import { Observable } from 'rxjs';
+import { RecipeBookComponent } from '../recipe-book/recipe-book.component';
+import { RecipesService } from '../recipes.service';
 
 @Component({
   selector: 'app-favorites',
@@ -10,6 +13,7 @@ export class FavoritesComponent implements OnInit {
   faHeart = faHeart;
   toggle: boolean = false;
   status = 'Enable';
+  recipeList: Observable<any> | null = null;
 
   constructor() {}
 
@@ -18,7 +22,13 @@ export class FavoritesComponent implements OnInit {
   enableDisable() {
     this.toggle = !this.toggle;
     this.status = this.toggle ? 'Enable' : 'Disable';
+    // if (!this.recipeList.recipebook){
+    //   recipeList.recipebook = true;
+    //   this.service.addRecipeBookListEvent(recipe);
+    // } else if (this.recipeList.recipebook === true) {
+    //   recipeList.recipebook = false;
+    //   this.service.removeRecipeBookListEvent(recipe);
+    // }
   }
 
-  
 }
--- src/app/recipe-book/recipe-book.component.ts
+++ src/app/recipe-book/recipe-book.component.ts
@@ -1,15 +1,25 @@
 import { Component, OnInit } from '@angular/core';
+import { faHeart, faTrash } from '@fortawesome/free-solid-svg-icons';
+import { RecipesService } from '../recipes.service';
+
 
 @Component({
   selector: 'app-recipe-book',
   templateUrl: './recipe-book.component.html',
-  styleUrls: ['./recipe-book.component.css']
+  styleUrls: ['./recipe-book.component.css'],
 })
 export class RecipeBookComponent implements OnInit {
+  faHeart = faHeart;
+  faTrash = faTrash;
+  recipeList: any[] | null = [];
 
-  constructor() { }
+  constructor(private service: RecipesService) {}
 
   ngOnInit(): void {
+    this.recipeList = this.service.getRecipeBookListEvent();
   }
 
+  removeRecipe(bucketEvent: any){
+    this.service.removeRecipeBookListEvent(bucketEvent);
+  };
 }
